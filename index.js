482f45d97408996ba0db58d94e84b9203eb5bf9d
GarayevF <112488747+GarayevF@users.noreply.github.com>
2025-01-04 18:56:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server"); 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
+server.get("/api/search", (req, res) => {
+  const query = req.query.q?.toLowerCase() || ""; 
+  const recipes = router.db.get("recipes").value(); 
+
+  // Sorguya göre filtreleme
+  const filteredRecipes = recipes.filter((recipe) => {
+    const { title, description, ingredients } = recipe;
+
+    return (
+      title.toLowerCase().includes(query) || 
+      description.toLowerCase().includes(query) || 
+      ingredients.some((ingredient) => ingredient.toLowerCase().includes(query))
+    );
+  });
+
+  res.json(filteredRecipes); 
+});
+
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
