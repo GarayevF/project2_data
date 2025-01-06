0b346c68d833f82229c3bb154e0a1488432c0402
GarayevF <112488747+GarayevF@users.noreply.github.com>
2025-01-06 04:04:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const jsonServer = require("json-server"); 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -12,8 +13,8 @@ server.use((req, res, next) => {
 });
 
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
 server.use(middlewares);
 
