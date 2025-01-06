819fa0d1b1e4aa32293e12ca7833edf70ed4c7dd
GarayevF <112488747+GarayevF@users.noreply.github.com>
2025-01-05 17:30:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jsonServer = require("json-server"); 
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
