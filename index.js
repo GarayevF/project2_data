0cf157ef1e6e44db4321955c568930231ba82d7f
GarayevF <112488747+GarayevF@users.noreply.github.com>
2025-01-05 18:15:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -139,6 +139,13 @@ server.patch("/api/update-order", (req, res) => {
 
 const port = process.env.PORT || 8080;
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 
